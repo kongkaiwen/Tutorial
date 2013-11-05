ec76c818fbb833d71ea29642f85aeab6e7968a47
Kevin <kfquinn@Three.(none)>
2013-11-04 20:44:06 -0700

    added skeleton

---
+++ tutorial/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorial::Application.config.secret_token = '13da02116cb488e6405fbc9a2a4e091daf3419d7d7bbdf1d3fcf97e5b400246e03f55cbc238f4ca2bc72e3cb4b60d92606661ca5905e2231eb77d49c4c07b709'
